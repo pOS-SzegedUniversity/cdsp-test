054be20a2f4037b8c77af353b0807290dd75e47c
fklosowski <1305367+fklosowski@users.noreply.github.com>
2025-12-08 11:57:14 +0100

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has title', async ({ page }) => {
-  await page.goto(process.env.TEST_APP_URL!);
+  await page.goto('https://fk-test-ci-instance-01.ps-01-platformos.com/');
 
   const heading = page.getByRole("heading")
   await expect(heading).toHaveText(/Contact Us/);
